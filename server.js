3aec5a09d2420f38c693255be1e6648a26a43826
Marko Janjic <marko.m.janjic@gmail.com>
2024-10-31 11:59:30 +0100

    setting up view engine,middleware, and public file paths

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 import express from "express";
+import path from "path";
+import url from "url";
+import session from "express-session";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.get("/", (req, res) => {
-  res.status(200).send("Home Page");
+  res.status(200).render("home");
 });
 
 app.listen(PORT, () => {
